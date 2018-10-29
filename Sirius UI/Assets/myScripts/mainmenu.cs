3065dbdc8b1ae6913d861fa50fe8394df39c1d7c
Paula <paula.navea.589@my.csun.edu>
2018-10-29 14:15:27 -0700

    UI Design

---
+++ Sirius UI/Assets/myScripts/mainmenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainmenu : MonoBehaviour
+{
+
+    public void quitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game Exits");
+    }
+
+    public void showPanel(GameObject gamee){
+        gamee.gameObject.SetActive(true);
+    }
+    public void hidePanel(GameObject gamee){
+        gamee.gameObject.SetActive(false);
+    }
+}
