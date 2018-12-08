4b5b1606a422a1125437421bd73d3e10dd52f001
Paula <paula.navea.589@my.csun.edu>
2018-12-07 17:48:34 -0800

    UI Updates
    updated the Crafting and the Inventory

--- Assets/Scripts/UIScripts/MainMenuScript.cs
+++ Assets/Scripts/UIScripts/MainMenuScript.cs
@@ -5,8 +5,34 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenuScript : MonoBehaviour {
-
+    public int resolut;
     public GameObject GameManager;
+    public Dropdown myDropdown;
+    public void SetDropdownIndex(Dropdown index)
+    {
+        int res = index.value;
+        resolut = res;
+    }
+public void screenSz()
+    {
+        switch (resolut)
+        {
+            case 0:
+                Screen.SetResolution(1024, 768, true);
+                Debug.Log("Screen size changed0");
+                break;
+
+            case 1:
+                Screen.SetResolution(1280, 720, true);
+                Debug.Log("Screen size changed1");
+                break;
+
+            case 2:
+                Screen.SetResolution(1600, 900, true);
+                Debug.Log("Screen size changed2");
+                break;
+        }
+    }
 
 
     public void NewGameButton()
