75639666767ed7494c6d7c255c10b976d1bd8fd5
Sixoul <ltwagner92@gmail.com>
2019-03-03 13:40:23 -0800

    Started dictionary class

---
+++ Assets/Scripts/InventoryScripts_v2/ItemDictionary.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDictionary : MonoBehaviour
+{
+
+    //Create the Dictionary
+    Dictionary<int, Item> items = new Dictionary<int, Item>();
+
+    // Use this for initialization
+    void Start()
+    {
+        //Adding item variables to be added to the dictionary
+        //Use an item constructor
+        //Item constructor consists of (String itemName, string itemDetail, Sprite icon, int cost, ItemTypes itemtype, int currentStack, int maxStack) 
+        Item dirt = new Item("Dirt");
+
+        //Add Variables to the dictionary
+        //items.Add("ID", <item variable from above>)
+        items.Add(1, dirt);
+    }
+
+    public Item getItem(int ID)
+    {
+        Debug.Log("ID sent: " + ID);
+        Debug.Log(items.Values);
+        return items[ID];
+    }
+}
--- Assets/Scripts/StaticObjScripts/Item.cs
+++ Assets/Scripts/StaticObjScripts/Item.cs
@@ -3,6 +3,12 @@ using System.Collections;
 
 public class Item : ScriptableObject
 {
+    public string name;
     public Sprite sprite;
     public GameObject itemObject;
+
+    public Item(string newName)
+    {
+        name = newName;
+    }
 }
