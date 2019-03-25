fc6cd9ec857e42df64683f7d2c915f93e9df711f
Sixoul <ltwagner92@gmail.com>
2019-03-24 19:41:02 -0700

    InputHandler
    Complete, may need changes for implementing in with the rest of the game.

---
+++ Assets/Scripts/InputManager/MenuScript.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuScript : MonoBehaviour
+{
+
+    Transform menuPanel;
+    Event keyEvent;
+
+    private Color32 unselected = new Color32(39, 171, 249, 255);
+    private Color32 selected = new Color32(239, 116, 36, 255);
+
+    Text buttonText;
+    GameObject currentKey;
+
+    bool waitingForKeyPress;
+
+    // Use this for initialization
+    void Start()
+    {
+        menuPanel = transform.FindChild("Panel");
+
+        for(int i = 0; i < menuPanel.GetChildCount(); ++i)
+        {
+            
+            if (menuPanel.GetChild(i).name == "Left")
+                menuPanel.GetChild(i).GetComponentInChildren<Text>().text = InputManager.instance.getKey("Left").ToString();
+            else if (menuPanel.GetChild(i).name == "Right")
+                menuPanel.GetChild(i).GetComponentInChildren<Text>().text = InputManager.instance.getKey("Right").ToString();
+            else if (menuPanel.GetChild(i).name == "Jump")
+                menuPanel.GetChild(i).GetComponentInChildren<Text>().text = InputManager.instance.getKey("Jump").ToString();
+            else if (menuPanel.GetChild(i).name == "PlayerAction")
+                menuPanel.GetChild(i).GetComponentInChildren<Text>().text = InputManager.instance.getKey("PlayerAction").ToString();
+            else if (menuPanel.GetChild(i).name == "PlayerAltAction")
+                menuPanel.GetChild(i).GetComponentInChildren<Text>().text = InputManager.instance.getKey("PlayerAltAction").ToString();
+
+            
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnGUI()
+    {
+        if(currentKey != null)
+        {
+            keyEvent = Event.current;
+            if(keyEvent.isKey)
+            {
+                InputManager.instance.setKey(currentKey.name, keyEvent.keyCode);
+                currentKey.GetComponentInChildren<Text>().text = keyEvent.keyCode.ToString();
+                currentKey.GetComponent<Image>().color = unselected;
+                currentKey = null;
+            }
+        }
+    }
+
+    public void ChangeKey(GameObject clicked)
+    {
+        if(currentKey != null)
+        {
+            currentKey.GetComponent<Image>().color = unselected;
+        }
+
+        currentKey = clicked;
+        currentKey.GetComponent<Image>().color = selected;
+    }
+
+}
