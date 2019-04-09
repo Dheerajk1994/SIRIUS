5cfc9a181f87fff10b815a38cd90ed96ee7b3f78
ryank95 <rkim_mail@yahoo.com>
2019-04-08 20:08:05 -0700

    modified speship scripts
    - changed interactable and engine

--- Assets/Scripts/ShipScripts/Interactable.cs
+++ Assets/Scripts/ShipScripts/Interactable.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class Interactable : MonoBehaviour
 {
+    public bool canInteract = false;
     public bool isInteracting = false;
+    public string interactText = "Press E to Interact";
+    Collider2D interactArea;
 
     public virtual void Interact()
     {
         Debug.Log("Interacting");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+            Interact();
     }
 }
