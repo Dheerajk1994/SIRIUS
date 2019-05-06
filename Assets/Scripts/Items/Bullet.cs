45c193088dd191152027f685c1bc92f762e77410
Dheerajk1994 <dheerajk.har@gmail.com>
2019-05-06 12:11:46 -0700

    added button to spawn scorpion

--- Assets/Scripts/Items/Bullet.cs
+++ Assets/Scripts/Items/Bullet.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bullet : MonoBehaviour {
+public class Bullet : MonoBehaviour { 
     public float speed = 200f;
     public Rigidbody2D rb;
     public float BulletDamage = 10f;
